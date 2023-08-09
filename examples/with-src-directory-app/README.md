a721727ae17e00ba06a964eefe6403828c44edf7
Mentor Kadriu <mentor.kadriu@jaywalker-digital.ch>
2023-08-09 10:58:10 +0200

    nextjs app with src directory

---
+++ examples/with-src-directory-app/README.md
@@ -0,0 +1,14 @@
+# Steps to reproduce the bug:
+Install nextjs and create a new app
+`npx create-next-app@latest`
+yes to `src` directory
+Add next-international
+`npm i next-international`
+Copy paste files from `next-app` to reproduce the bug.
+```bash
+npm run dev
+```
+# Work with the bug:
+```bash
+npm i && npm run dev
+```
---
+++ examples/with-src-directory-app/src/app/[locale]/page.tsx
@@ -0,0 +1,80 @@
+import { getI18n, getScopedI18n, getCurrentLocale, getStaticParams } from '../../../locales/server';
+
+// Only needed for SSG
+// export const generateStaticParams = getStaticParams();
+
+export default async function Home() {
+  const t = await getI18n();
+  const t2 = await getScopedI18n('scope.more');
+  const locale = getCurrentLocale();
+
+  return (
+    <div>
+      <h1>SSR / SSG</h1>
+      <p>
+        Current locale:
+        <span>{locale}</span>
+      </p>
+      <p>Hello: {t('hello')}</p>
+      <p>
+        Hello:{' '}
+        {t('welcome', {
+          name: 'John',
+        })}
+      </p>
+      <p>
+        Hello (with React components):{' '}
+        {t('welcome', {
+          name: <strong>John</strong>,
+        })}
+      </p>
+      <p>
+        Hello:{' '}
+        {t('about.you', {
+          age: '23',
+          name: 'Doe',
+        })}
+      </p>
+      <p>
+        Hello (with React components):{' '}
+        {t('about.you', {
+          age: <strong>23</strong>,
+          name: 'Doe',
+        })}
+      </p>
+      <p>{t2('test')}</p>
+      <p>
+        {t2('param', {
+          param: 'test',
+        })}
+      </p>
+      <p>
+        {t2('param', {
+          param: <strong>test</strong>,
+        })}
+      </p>
+      <p>{t2('and.more.test')}</p>
+      <p>{t('missing.translation.in.fr')}</p>
+      <p>
+        {t('cows', {
+          count: 1,
+        })}
+      </p>
+      <p>
+        {t('cows', {
+          count: 2,
+        })}
+      </p>
+      <p>
+        {t2('stars', {
+          count: 1,
+        })}
+      </p>
+      <p>
+        {t2('stars', {
+          count: 2,
+        })}
+      </p>
+    </div>
+  );
+}
